a004f59a15b13e8d945d83c64ef237a19b5bb764
Pablox5250 <pcastillogarcera@gmail.com>
2025-09-30 18:46:16 +0100

    Update Agenda.java

--- src/org/ulpgc/is1/model/Agenda.java
+++ src/org/ulpgc/is1/model/Agenda.java
@@ -1,5 +1,23 @@
 package org.ulpgc.is1.model;
 
 public class Agenda {
+    public String addPerson(){
 
+    }
+
+    public String addCompany(){
+
+    }
+
+    public String addGroup(){
+
+    }
+
+    public String getContactList(){
+
+    }
+
+    public String getGroupList(){
+
+    }
 }
