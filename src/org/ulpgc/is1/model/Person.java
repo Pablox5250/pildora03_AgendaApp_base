1b626c9c517f7611db3d6214f93de8bc0a0b64bd
Pablox5250 <pcastillogarcera@gmail.com>
2025-09-30 18:47:47 +0100

    Update Person.java

--- src/org/ulpgc/is1/model/Person.java
+++ src/org/ulpgc/is1/model/Person.java
@@ -1,5 +1,11 @@
 package org.ulpgc.is1.model;
 
 public class Person extends Contact {
+    public String firstName;
+    public String lastName;
 
+    @Override
+    public String getName() {
+        return "";
+    }
 }
