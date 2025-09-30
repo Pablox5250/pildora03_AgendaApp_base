5c7010a3ab6c152a72482e3082e8d8770e85aa45
Pablox5250 <pcastillogarcera@gmail.com>
2025-09-30 18:47:03 +0100

    Update Contact.java

--- src/org/ulpgc/is1/model/Contact.java
+++ src/org/ulpgc/is1/model/Contact.java
@@ -1,5 +1,13 @@
 package org.ulpgc.is1.model;
 
 public abstract class Contact {
+    public String telephone;
+    public String email;
 
+    public Contact(String telephone, String email) {
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+    public abstract String getName();
 }
